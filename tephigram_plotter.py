7978195e011bd0cda9025224dbf0057acf579e06
Leif Denby <leif@denby.eu>
2014-08-09 11:49:52 +0100

    Added RH-plotting

--- tephigram_plotter.py
+++ tephigram_plotter.py
@@ -41,6 +41,8 @@ T1=-10.
 T2=40.
 theta1=-12.
 
+plot_sat_adiabat_labels = False
+
 rotation_angle = 45.
 rotation_origin = [T1, theta1]
 
@@ -52,20 +54,25 @@ qs_ = 1.e-3*np.array([30., 20., 15., 10., 7., 5., 3., 2., 15., 1.0, 0.7,])
 x_min = -40.
 x_max = 60.0
 y_min = -10.
-y_max = 100.
+y_max = 80.
 
 T_ = np.arange(T_min, T_max+0.1, d_T)
 
 class Tephigram:
-    def __init__(self):
-        fig = plot.figure(figsize=(10,10))
+    def __init__(self, fig=None, subplotshape=None):
+        if fig is None:
+            fig = plot.figure(figsize=(10,10))
         self.fig = fig
-        self.setup_axes1(fig, '111')
+        self.setup_axes1(fig, subplotshape)
+        self.plot = plot
+        self.lines = []
+
         self.plot_temp_lines()
         self.plot_pot_temp_lines()
         self.plot_pressure_lines()
         self.plot_qs_lines()
         self.plot_sat_adiabats()
+
 
     def savefig(self, filename):
         self.fig.savefig(filename)
@@ -84,8 +91,31 @@ class Tephigram:
         theta = self.f_theta(P=P, T=(T_dp+273.15))
         self.ax1.plot(*self._tf(T_dp, theta-273.15), marker='o', color='green')
 
+    def plot_temp(self, P, T, color='red'):
+        """
+        Input sounding defined by P and T
 
-    def setup_axes1(self, fig, rect):
+        Expected units:
+            T [C], P [hPa]
+        """
+        theta = self.f_theta(P=P, T=(T+273.15))
+
+        x, y = self._tf(T, theta-273.15)
+
+        self.ax1.plot(*self._tf(T, theta-273.15), marker='o', color=color)
+
+        return x, y
+
+    def plot_RH(self, P, T, RH, color='green'):
+        """
+        dew-point equation source: http://andrew.rsmas.miami.edu/bmcnoldy/Humidity.html
+        """
+        T_dp = 243.04*(np.log(RH)+((17.625*T)/(243.04+T)))/(17.625-np.log(RH)-((17.625*T)/(243.04+T)))
+
+        theta = self.f_theta(P=P, T=(T_dp+273.15))
+        self.ax1.plot(*self._tf(T_dp, theta-273.15), marker='o', color='green')
+
+    def setup_axes1(self, fig, subplotshape=None):
         """
         A simple one.
         """
@@ -97,7 +127,10 @@ class Tephigram:
 
         grid_helper = GridHelperCurveLinear(self.tr, grid_locator1=FixedLocator(T_ticks), grid_locator2=FixedLocator(theta_ticks))
 
-        ax1 = Subplot(fig, 1, 1, 1, grid_helper=grid_helper)
+        if subplotshape is None:
+            subplotshape = (1,1,1)
+
+        ax1 = Subplot(fig, *subplotshape, grid_helper=grid_helper)
         # ax1 will have a ticks and gridlines defined by the given
         # transform (+ transData of the Axes). Note that the transform of
         # the Axes itself (i.e., transData) is not affected by the given
@@ -136,7 +169,9 @@ class Tephigram:
     def plot_pot_temp_lines(self):
         theta_ = np.arange(theta_min, theta_max, d_theta)
         for theta in theta_:
-            self.ax1.plot(*self._tf([T_min, T_max], [theta, theta]  ), linestyle=':', color='green')
+            line = self.ax1.plot(*self._tf([T_min, T_max], [theta, theta]  ), linestyle=':', color='green', label='dry adiabat')
+
+        self.lines += line
 
     def plot_pressure_lines(self):
         """
@@ -223,7 +258,7 @@ class Tephigram:
             theta_const_qs = np.array(theta_const_qs_K) - 273.15
             
             x, y = self._tf(T, theta_const_qs)
-            self.ax1.plot(x, y, linestyle='-.', color='black')
+            line = self.ax1.plot(x, y, linestyle='-.', color='black', label='moist adiabat')
 
             k = np.argmin(np.abs(np.array(P_arr)-1000.))
             T_at_1000 = T[k]
@@ -233,7 +268,14 @@ class Tephigram:
             x0 = x[kk] + 1.
             y0 = y[kk] - 2.
 
-            self.ax1.text(x0, y0, "%gC" % T_at_1000, color='black')
+            if plot_sat_adiabat_labels:
+                self.ax1.text(x0, y0, "%gC" % T_at_1000, color='black')
+
+        self.lines += line
+
+    def plot_legend(self):
+        labels = [l.get_label() for l in self.lines]
+        plot.legend(self.lines, labels, loc='upper left', prop={'size': 10})
         
 
 if __name__ == "__main__":
@@ -246,4 +288,17 @@ if __name__ == "__main__":
 
     tephigram.plot_sounding(P=P, T=T, T_dp=T_dp)
 
+    RH = sounding[4]
+    T_dp2 = 243.04*(np.log(RH/100)+((17.625*T)/(243.04+T)))/(17.625-np.log(RH/100)-((17.625*T)/(243.04+T)))
+    tephigram.plot_sounding(P=P, T=T, T_dp=T_dp2)
+
     tephigram.savefig('tephigram_example.png')
+
+
+    plot.figure()
+    z = sounding[1]
+
+    plot.plot(T_dp2, z, label='calc')
+    plot.plot(T_dp, z)
+    plot.legend()
+    raw_input()
